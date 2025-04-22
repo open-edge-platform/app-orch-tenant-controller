e81a3785c546da7481112b76ba9dbace2e7ea100
Ray Milkey <ray.milkey@gmail.com>
2025-04-22 14:31:40 -0700

    Remove unused context parameter

--- internal/plugins/plugin.go
+++ internal/plugins/plugin.go
@@ -75,7 +75,7 @@ func Dispatch(ctx context.Context, event Event, hook *nexushook.Hook) error {
 	log.Infof("Done dispatching event: %v", event)
 	if event.EventType == "create" {
 		if hook != nil {
-			err = hook.UpdateProjectManifestTag(ctx, event.Project)
+			err = hook.UpdateProjectManifestTag(event.Project)
 			if err != nil {
 				return err
 			}
